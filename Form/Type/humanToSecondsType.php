802d1a32e9cae6fd803bc02eff3ce30c2a5f0929
Leevi Graham <leevi@newism.com.au>
2012-07-23 00:01:38 +1000

    Upgrade to Symfony 2.1 methods Fixed namespace

--- Form/Type/humanToSecondsType.php
+++ Form/Type/humanToSecondsType.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace NSM\Bundle\NSMDateTimeBundle\Form\Type;
+namespace NSM\Bundle\DateTimeBundle\Form\Type;
 
 use NSM\Bundle\NSMDateTimeBundle\Form\DataTransformer\HumanToSecondsTransformer;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 
@@ -14,15 +14,15 @@ class HumanToSecondsType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->appendClientTransformer(new HumanToSecondsTransformer());
+        $builder->addViewTransformer(new HumanToSecondsTransformer());
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getParent(array $options)
+    public function getParent()
     {
         return 'text';
     }
